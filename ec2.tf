3bd696a5753393f94a5614c9c4640679cc8b301a
Konicasethi16 <167297988+Konicasethi16@users.noreply.github.com>
2025-09-05 15:35:15 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -6,3 +6,19 @@ resource aws_instance web {
   ami = "ami-0861f4e788f5069dd"
   
 }
+resource "aws_ebs_volume" "data_disk" {
+  availability_zone = "ap-south-1a"
+  size              = 20 # 20 GB
+  type              = "gp3"
+
+  tags = {
+    Name = "demo-data-disk"
+  }
+}
+
+# Attach the EBS volume to the EC2 instance
+resource "aws_volume_attachment" "data_disk_attach" {
+  device_name = "/dev/sdf" # Linux will map it to /dev/xvdf
+  volume_id   = aws_ebs_volume.data_disk.id
+  instance_id = aws_instance.web.id
+}
